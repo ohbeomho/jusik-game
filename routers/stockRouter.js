15ff8141b716ad8f759ef3ccce29db14d987fd8d
OhBeomho <ohbeomho0322@gmail.com>
2024-03-03 16:57:33 +0900

    buying/selling stocks

--- routers/stockRouter.js
+++ routers/stockRouter.js
@@ -1,11 +1,126 @@
 import { Router } from "express";
 import prisma from "../prisma";
+import wrap from "../utils/asyncWrapper";
 
 const router = Router();
 
-// TODO
-router.get("/buy");
+router.use((req, res, next) => {
+  if (!req.session.user) {
+    res.sendStatus(401);
+    return;
+  }
 
-router.get("/sell");
+  next();
+});
+
+function getStock(id) {
+  return prisma.stock.findUniqueOrThrow({
+    where: {
+      id
+    },
+    select: {
+      id: true,
+      priceHistory: true,
+      currentPrice: true
+    }
+  });
+}
+
+function getUserStock(stockId, username) {
+  return prisma.userStock.findUnique({
+    where: {
+      stockId,
+      username
+    },
+    select: {
+      id: true,
+      quantity: true,
+      totalCredits: true
+    }
+  });
+}
+
+router.post(
+  "/buy",
+  wrap(async (req, res) => {
+    const { stockId, quantity } = req.body;
+    const username = req.session.user;
+    const stock = await getStock(stockId);
+    const userStock = await getUserStock(stockId, username);
+
+    if (userStock) {
+      await prisma.userStock.update({
+        where: {
+          id: userStock.id
+        },
+        data: {
+          totalCredits: { increment: stock.currentPrice * quantity },
+          quantity: {
+            increment: quantity
+          }
+        }
+      });
+    } else {
+      await prisma.userStock.create({
+        data: {
+          quantity,
+          totalCredits: stock.currentPrice * quantity,
+          username,
+          stockId
+        }
+      });
+    }
+
+    res.sendStatus(200);
+  })
+);
+
+router.get(
+  "/sell",
+  wrap(async (req, res) => {
+    const { stockId, quantity } = req.body;
+    const username = req.session.user;
+    const stock = await getStock(stockId);
+    const userStock = await getUserStock(stockId, username);
+
+    if (!userStock) {
+      throw { message: "주식을 보유하고 있지 않습니다.", code: 400 };
+    }
+
+    if (quantity >= userStock.quantity) {
+      await prisma.user.update({
+        where: {
+          username
+        },
+        data: {
+          credits: {
+            increment: userStock.totalCredits
+          }
+        }
+      });
+      await prisma.userStock.delete({
+        where: {
+          id: userStock.id
+        }
+      });
+    } else {
+      await prisma.userStock.update({
+        where: {
+          id: userStock.id
+        },
+        data: {
+          quantity: {
+            decrement: quantity
+          },
+          totalCredits: {
+            decrement: stock.currentPrice * quantity
+          }
+        }
+      });
+    }
+
+    res.sendStatus(200);
+  })
+);
 
 export default router;
